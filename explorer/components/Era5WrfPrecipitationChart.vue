48634d11ace92db418e3156a0fc8f641985f0ebd
Charles Parr <cparr4@alaska.edu>
2025-11-04 14:08:35 -0900

    add precip time series chart

---
+++ explorer/components/Era5WrfPrecipitationChart.vue
@@ -0,0 +1,77 @@
+<script lang="ts" setup>
+import type { Data } from 'plotly.js-dist-min'
+import type { Era5WrfSeriesPoint } from '~/utils/era5WrfTransforms'
+import { CHART_CONFIG, ERA5_WRF_VARIABLES } from '~/utils/era5WrfConstants'
+
+interface Props {
+  rainnc: Era5WrfSeriesPoint[]
+  lat: number
+  lng: number
+  chartId?: string
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  chartId: 'era5-precipitation-chart',
+})
+
+const { $Plotly } = useNuxtApp()
+const chartId = props.chartId
+
+const rainncVar = ERA5_WRF_VARIABLES.find(v => v.key === 'rainnc_sum')!
+
+const buildChart = () => {
+  const traces: Data[] = [
+    {
+      x: props.rainnc.map(p => p.date),
+      y: props.rainnc.map(p => p.value),
+      name: rainncVar.label,
+      type: 'bar',
+      marker: { color: rainncVar.color, opacity: 0.7 },
+    },
+  ]
+
+  $Plotly.newPlot(
+    chartId,
+    traces,
+    {
+      title: {
+        text: `Daily Total Precipitation<br><sub>${props.lat.toFixed(3)}°N, ${props.lng.toFixed(3)}°W</sub>`,
+        font: { size: 24 },
+      },
+      xaxis: {
+        title: {
+          text: 'Date',
+          font: { size: 18 },
+          standoff: 20,
+        },
+        type: 'date',
+        fixedrange: true,
+      },
+      yaxis: {
+        title: {
+          text: 'Precipitation (mm)',
+          font: { size: 18 },
+        },
+        rangemode: 'tozero',
+      },
+      ...CHART_CONFIG.layout,
+    },
+    CHART_CONFIG.plotlyOptions
+  )
+}
+
+watch(() => props.rainnc, buildChart, { deep: true })
+
+onMounted(buildChart)
+onUnmounted(() => $Plotly.purge(chartId))
+</script>
+
+<template>
+  <div :id="chartId" class="era5-chart" />
+</template>
+
+<style scoped>
+.era5-chart {
+  min-height: 400px;
+}
+</style>
