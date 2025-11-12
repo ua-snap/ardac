9e53b027b206e98e29e41e6356c3c71aa15677bd
Bob Torgerson <rltorgerson@alaska.edu>
2025-11-12 13:49:38 -0900

    Adds slot 'additionalInstructions' to Gimme component.

--- explorer/components/Gimme.vue
+++ explorer/components/Gimme.vue
@@ -292,6 +292,7 @@ onUnmounted(() => {
           <span v-if="communitiesEnabled">a community or by</span>
           lat/long</label
         >
+        <slot name="additionalInstructions" />
         <p v-if="communitiesEnabled" class="is-size-5">
           Only communities within the footprint of the data are included in this
           search.
