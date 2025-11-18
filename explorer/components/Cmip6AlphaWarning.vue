f229cc004d3469528985aa48b781a03a43a5b93b
Craig Stephenson <crstephenson@alaska.edu>
2025-11-18 10:56:23 -0900

    Remove space before period in CMIP6 alpha warning.

--- explorer/components/Cmip6AlphaWarning.vue
+++ explorer/components/Cmip6AlphaWarning.vue
@@ -3,7 +3,7 @@
     <span class="warn">⚠️</span> Note that this dataset is a
     <strong>pre-release</strong> and is offered as a way to see and explore this
     upcoming dataset, but
-    <strong> isn&rsquo;t ready to use for research or decision making </strong>.
+    <strong> isn&rsquo;t ready to use for research or decision making</strong>.
     Please
     <a href="mailto:uaf-snap-data-tools@alaska.edu">get in touch</a> about
     similar data which are peer reviewed.
